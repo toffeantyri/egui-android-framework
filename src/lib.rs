87e53d7bdded58fb3a769673b566255210a7f3e7
toffeantyri <toffeantyri@gmail.com>
2026-06-22 17:56:10 +0500

    fix test: add activity test

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub use view_model::{ViewModel, ViewModelContext};
 
 #[cfg(test)]
 mod tests {
+    mod activity_tests;
     mod error_tests;
     mod lifecycle_tests;
     mod view_model_tests;
---
+++ src/tests/activity_tests.rs
@@ -0,0 +1,55 @@
+use crate::{Activity, AppConfig, AppContext, Application, LifecycleObserver, ViewModel, activity};
+
+enum DummyCmd {}
+enum DummyEvt {}
+
+struct DummyVM;
+
+impl ViewModel for DummyVM {
+    type DataCommand = DummyCmd;
+    type Event = DummyEvt;
+
+    fn create(_context: crate::ViewModelContext<Self::DataCommand, Self::Event>) -> Self {
+        Self
+    }
+}
+
+struct DummyApp;
+impl Application for DummyApp {
+    type Activity = DummyActivity;
+
+    type ViewModel = DummyVM;
+
+    fn on_create(_context: &mut crate::AppContext<Self>) {}
+
+    fn create_view_model(_context: &mut crate::AppContext<Self>) -> Self::ViewModel {
+        DummyVM
+    }
+
+    fn create_activity(_context: &mut crate::AppContext<Self>) -> Self::Activity {
+        DummyActivity
+    }
+}
+
+struct DummyActivity;
+
+impl Activity for DummyActivity {
+    type ViewModel = DummyVM;
+
+    type Application = DummyApp;
+
+    fn create(_context: &crate::AppContext<Self::Application>) -> Self {
+        Self
+    }
+
+    fn render(&mut self, _context: &egui::Context, _vm: &Self::ViewModel) {}
+}
+
+impl LifecycleObserver for DummyActivity {}
+
+#[test]
+fn test_activity_creation() {
+    let mut ctx = AppContext::<DummyApp>::new();
+    let activity = DummyActivity::create(&ctx);
+    //если не паникует значит все Ок
+}
